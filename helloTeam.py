bd4677f53cdb3b79048368c2234cf6bc92d3483b
bintadpatel <43495446+bintadpatel@users.noreply.github.com>
2018-10-06 12:50:40 -0400

    Add files via upload

--- helloTeam.py
+++ helloTeam.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask (__name__)
 @app.route("/")
 def hello():
-    return "Hello World! This is Team Accenture's first flask app"
+    return "Hello World! This is Team Accenture's first flask app. We are testing ATTEMPT 2!"
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port=9000)
