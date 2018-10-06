388a2516018b957afb4c002acc0af84965a86843
bintadpatel <43495446+bintadpatel@users.noreply.github.com>
2018-10-06 12:40:43 -0400

    Add files via upload

---
+++ helloTeam.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask (__name__)
+@app.route("/")
+def hello():
+    return "Hello World! This is Team Accenture's first flask app"
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port=9000)
+    
+    
+    ##references
+    ## http://www.patricksoftwareblog.com/steps-for-starting-a-new-flask-project-using-python3/
