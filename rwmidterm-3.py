2142070648e92c4756dcbf58efd716dc92558776
rjwillia76git <richard.williams.76@gmail.com>
2018-11-03 09:05:02 -0400

    Update rwmidterm-3.py

--- rwmidterm-3.py
+++ rwmidterm-3.py
@@ -1,8 +1,4 @@
-
-# coding: utf-8
-
-# In[1]:
-
+# Libraries
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,16 +11,9 @@ import plotly.graph_objs as go
 import io
 import base64 
 
-
-# In[2]:
-
-
 app = Flask (__name__)
 
-
-# In[3]:
-
-
+# Defining the function that will pull historical stock data from Yahoo! Finance
 def get_adj_close(ticker, start, end):
         '''
         A function that takes ticker symbols, starting period, ending period
@@ -36,34 +25,11 @@ def get_adj_close(ticker, start, end):
         info = web.DataReader(ticker, data_source='yahoo', start=start, end=end)['Adj Close']
         return pd.DataFrame(info)
 
-
-# In[ ]:
-
-
-# def bb(ticker): 
-#     # Get Adjusted Closing Prices for Input and Tesla between 2016-2017
-#         tick1 = get_adj_close(ticker, '1/2/2017', '26/10/2018')
-#         tesla = get_adj_close('tsla', '1/2/2017', '26/10/2018')
-#     # Calculate 30 Day Moving Average, Std Deviation, Upper Band and Lower Band
-#         for item in (tick1, tesla):
-#             item['30 Day MA'] = item['Adj Close'].rolling(window=20).mean()
-#             item['30 Day STD'] = item['Adj Close'].rolling(window=20).std()
-#             item['Upper Band'] = item['30 Day MA'] + (item['30 Day STD'] * 2)
-#             item['Lower Band'] = item['30 Day MA'] - (item['30 Day STD'] * 2)
-#     # Simple 30 Day Bollinger Band for Facebook (2016-2017)
-#         tick1[['Adj Close', '30 Day MA', 'Upper Band', 'Lower Band']].plot(figsize=(12,6))
-#         plt.title('30 Day Bollinger Band')
-#         plt.ylabel('Price (USD)')
-#         plt.show();
-
-
-# In[4]:
-
-
+# Defining the Bollinger Band Function
 def bb(ticker): 
-    # Get Adjusted Closing Prices for Input and Tesla between 2016-2017
-        tick1 = get_adj_close(ticker, '1/2/2017', '26/10/2018')
-        tesla = get_adj_close('tsla', '1/2/2017', '26/10/2018')
+    # Get Adjusted Closing Prices for Chosen Stock and Tesla between 2017-October 2018
+        tick1 = get_adj_close(ticker, '1/2/2017', '31/10/2018')
+        tesla = get_adj_close('tsla', '1/2/2017', '31/10/2018')
     # Calculate 30 Day Moving Average, Std Deviation, Upper Band and Lower Band
         for item in (tick1, tesla):
             item['30 Day MA'] = item['Adj Close'].rolling(window=20).mean()
@@ -80,32 +46,19 @@ def bb(ticker):
         plot_url = base64.b64encode(img.getvalue()).decode()
         return '<img src="data:image/png;base64,{}">'.format(plot_url)
 
-
-# In[5]:
-
-
+#Flask Apps
 @app.route("/")
 def index():
-    return "Welcome to my flask app! Please type in http://18.222.187.92:5000/stocks/{enter ticker symbol} to see the price of the stock you enter.  For example, if you want to see the price of Accenture's stock, enter http://18.222.187.92:5000/stocks/ACN"
-
-
-# In[6]:
-
+    return "Welcome to my flask app! Please type in http://18.222.187.92:5000/stocks/{enter ticker symbol} to see the Bollinger Band for the stock you enter.  For example, if you want to see the Bollinger Bands of Accenture's stock, enter http://18.222.187.92:5000/stocks/ACN"
 
 @app.route("/stocks")
 def stocks():
     return "Stocks"
 
-
-# In[7]:
-
-
+# User enters a ticker symbol in the URL, and the string name is passed through to the bb(ticker) function to return the Bollinger Bands.
 @app.route("/stocks/<string:name>/")
 def getStock(name):
     return bb(name)
-
-
-# In[ ]:
 
 
 if __name__ == "__main__":
