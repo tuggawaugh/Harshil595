b3bd26681574880f9c8b11069707ba56b30915fb
bintadpatel <bintapatel@gmail.com>
2018-11-04 21:42:56 -0500

    FE595midtermBPv2
    testing midterm code

--- FE595midtermBPv2.py
+++ FE595midtermBPv2.py
@@ -18,7 +18,7 @@ import base64
 # In[ ]:
 
 
-# creates the flask app
+# create the flask app
 from flask import Flask
 app = Flask (__name__)
 
@@ -26,9 +26,8 @@ app = Flask (__name__)
 # In[ ]:
 
 
-# define a function that returns a dataframe that returns data from yahoo based on selected stock symbol
+# define a function that returns a dataframe of data from yahoo based on selected stock symbol
 def get_adj_close(ticker, start, end):
-    
         start = start
         end = end
         info = web.DataReader(ticker, data_source='yahoo', start=start, end=end)['Adj Close']
@@ -38,64 +37,64 @@ def get_adj_close(ticker, start, end):
 # In[ ]:
 
 
+# create the historical trend and moving average plot based on ticker input
+
 def stockplot(ticker):
     img = io.BytesIO()
-    tick1 = get_adj_close(ticker, '01/01/2016', '31/12/2017')
-    tick1[['Adj Close']].plot(figsize=(12,6)) 
-    plt.title('Price History')
+    tick1['Adj Close'] = get_adj_close(ticker, '01/01/2016', '31/12/2017')
+    # tick1[['Adj Close']].plot(figsize=(10,10)) 
+    df['ma50'] = pd.rolling_mean(df['Adj Close'], 50)
+    plots = df[['Adj Close', 'ma50']].plot(subplots=False, figsize=(10, 10))
+    plt.title('Historical Price Trend')
     plt.ylabel('Price (USD)')
     plt.savefig(img, format='png')
     img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()
-    return '<img src="data:image/png;base64,{}">'.format(plot_url)
-    
-# reference https://ntguardian.wordpress.com/2016/09/19/introduction-stock-market-data-python-1/
+    plots = base64.b64encode(img.getvalue()).decode()
+    return plt.show()
 
 
 # In[ ]:
 
 
-# obtain ticker date for the set time period from quandl
+#def stockplot(ticker):
+#    img = io.BytesIO()
+#    tick1 = get_adj_close(ticker, '01/01/2016', '31/12/2017')
+#    tick1[['Adj Close']].plot(figsize=(10,10)) 
+#    plt.title('Historical Price Trend')
+#    plt.ylabel('Price (USD)')
+#    plt.savefig(img, format='png')
+#    img.seek(0)
+#    plots = base64.b64encode(img.getvalue()).decode()
+#    return '<img src="data:image/png;base64,{}">'.format(plots)
+    
+        
+# References 
+# https://ntguardian.wordpress.com/2018/07/17/stock-data-analysis-python-v2/
+# https://scotch.io/bar-talk/processing-incoming-request-data-in-flask
+
+
+# In[ ]:
+
+
+# obtain ticker data for the set time period from yahoo
 @app.route("/stocks/<string:name>/")
 def getStock(name):
-       return stockplot(name)
-    
-
-
-# In[ ]:
-
-
-# #from Gordon's example to return output
-
-@app.route('/query-example')
-def query_example():
     language = request.args.get('language') #if key doesn't exist, returns None
     language = language.lower()
     framework = request.args.get('framework')
     ticker = request.args['ticker'] #if key doesn't exist, returns a 400, bad request error 
-    return '''<h1>The language value is: {}</h1>
+    return stockplot(name)
+    '''<h1>The language value is: {}</h1>
     <h1>The framework value is: {}</h1>
     <h1>The ticker value is: {}'''.format(language, framework,ticker)
+  
 
 
 # In[ ]:
 
 
-# # run the flask app on port 9000 on the localhost
-# if __name__ == "__main__":
-#     app.run('localhost', 9000, app)
-
-
-# In[ ]:
-
+# run the flask app on port 9000 on the localhost
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port=9000)
-
-
-# In[ ]:
-
-
-# References
-# https://scotch.io/bar-talk/processing-incoming-request-data-in-flask
 
