2dc282c3d9ce49631cf996fbfbbc530d6b604d06
bintadpatel <bintapatel@gmail.com>
2018-11-04 21:58:02 -0500

    FE595midtermBPv2
    testing midterm changes

--- FE595midtermBPv2.py
+++ FE595midtermBPv2.py
@@ -27,6 +27,7 @@ app = Flask (__name__)
 
 
 # define a function that returns a dataframe of data from yahoo based on selected stock symbol
+
 def get_adj_close(ticker, start, end):
         start = start
         end = end
@@ -42,32 +43,19 @@ def get_adj_close(ticker, start, end):
 def stockplot(ticker):
     img = io.BytesIO()
     tick1['Adj Close'] = get_adj_close(ticker, '01/01/2016', '31/12/2017')
-    # tick1[['Adj Close']].plot(figsize=(10,10)) 
-    df['ma50'] = pd.rolling_mean(df['Adj Close'], 50)
+    tick1[['Adj Close']].plot(figsize=(10,10)) 
+    #df['ma50'] = pd.rolling_mean(df['Adj Close'], 50)
     plots = df[['Adj Close', 'ma50']].plot(subplots=False, figsize=(10, 10))
     plt.title('Historical Price Trend')
     plt.ylabel('Price (USD)')
     plt.savefig(img, format='png')
     img.seek(0)
     plots = base64.b64encode(img.getvalue()).decode()
-    return plt.show()
+    return '<img src="data:image/png;base64,{}">'.format(plots)
 
+#    tick1 = get_adj_close(ticker, '1/2/2017', '26/10/2018')
+#    tick1[['Adj Close']].plot(figsize=(12,6)) 
 
-# In[ ]:
-
-
-#def stockplot(ticker):
-#    img = io.BytesIO()
-#    tick1 = get_adj_close(ticker, '01/01/2016', '31/12/2017')
-#    tick1[['Adj Close']].plot(figsize=(10,10)) 
-#    plt.title('Historical Price Trend')
-#    plt.ylabel('Price (USD)')
-#    plt.savefig(img, format='png')
-#    img.seek(0)
-#    plots = base64.b64encode(img.getvalue()).decode()
-#    return '<img src="data:image/png;base64,{}">'.format(plots)
-    
-        
 # References 
 # https://ntguardian.wordpress.com/2018/07/17/stock-data-analysis-python-v2/
 # https://scotch.io/bar-talk/processing-incoming-request-data-in-flask
@@ -77,16 +65,18 @@ def stockplot(ticker):
 
 
 # obtain ticker data for the set time period from yahoo
-@app.route("/stocks/<string:name>/")
-def getStock(name):
+
 #    language = request.args.get('language') #if key doesn't exist, returns None
 #    language = language.lower()
 #    framework = request.args.get('framework')
 #    ticker = request.args['ticker'] #if key doesn't exist, returns a 400, bad request error 
-    return stockplot(name)
 #    '''<h1>The language value is: {}</h1>
 #    <h1>The framework value is: {}</h1>
 #    <h1>The ticker value is: {}'''.format(language, framework,ticker)
+
+@app.route("/stocks/<string:name>/")
+def getStock(name):
+       return stockplot(name)
   
 
 
