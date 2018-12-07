e153777f234ed8e61e4fb1b50303e714fa9ca916
Gordon Rubin <gordon.rubin@accenture.com>
2018-12-07 00:45:43 -0500

    limit to NYSE securities to align with pandas data reader stock availability
    (via yahoo)

--- final/frontendproject/project/midterms/grubin.py
+++ final/frontendproject/project/midterms/grubin.py
@@ -61,6 +61,33 @@ def get_adj_close(ticker, start, end):
             print("No information for ticker '%s'" % ticker)
             return -1
 
+def rsiFunc(prices, n=14):
+    deltas = np.diff(prices)
+    seed = deltas[:n+1]
+    up = seed[seed>=0].sum()/n
+    down = -seed[seed<0].sum()/n
+    rs = up/down
+    rsi = np.zeros_like(prices)
+    rsi[:n] = 100. - 100./(1.+rs)
+
+    for i in range(n, len(prices)):
+        delta = deltas[i-1] # cause the diff is 1 shorter
+
+        if delta>0:
+            upval = delta
+            downval = 0.
+        else:
+            upval = 0.
+            downval = -delta
+
+        up = (up*(n-1) + upval)/n
+        down = (down*(n-1) + downval)/n
+
+        rs = up/down
+        rsi[i] = 100. - 100./(1.+rs)
+
+    return rsi
+
 
 
 
@@ -69,9 +96,8 @@ def bb(ticker):
         # Get Adjusted Closing Prices for Input
         tick1 = get_adj_close(ticker, '1/1/2016', '31/12/2017')
 
-        # Calculate RSI
-        x = np.arange(0, 10, 0.2)
-        y = np.sin(x)
+        # Calculate RSI        
+        tick1['rsi'] = rsiFunc(tick1['Adj Close'], 14)
         
         img = io.BytesIO()
 
@@ -80,11 +106,12 @@ def bb(ticker):
         ax0 = plt.subplot(gs[0])
         ax0.plot(tick1[['Adj Close']])
         ax1 = plt.subplot(gs[1])
-        ax1.plot(tick1[['Adj Close']])
+        ax1.plot(tick1['rsi'])
+        fig.autofmt_xdate()
         
 #        plt.tight_layout()
  
-        plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=.5)
+        plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
 #        
 #        left  = 0.125  # the left side of the subplots of the figure
 #        right = 0.9    # the right side of the subplots of the figure
