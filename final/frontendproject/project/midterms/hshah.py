f134a8b60ccaa36f5dfda98150cae1920fa147f7
tuggawaugh <tuggawaugh@users.noreply.github.com>
2018-12-10 22:34:10 -0500

    updated HTML title

--- final/frontendproject/project/midterms/hshah.py
+++ final/frontendproject/project/midterms/hshah.py
@@ -78,7 +78,7 @@ def stocks_hshah():
                 <h1>No entry for Ticker Symbol: {}</h1>'''.format(ticker) 
     else:
         return '''
-                <title>Comparsion with S&P 500 </title>
+                <title>Comparative Analysis (vs. S&P 500) </title>
                 <h1>Price Comparison with S&P 500 for Ticker: {}  ({} - {})</h1>
                 <h3>Midterm Project: Harshil Shah</h3>
 				<form method="POST">
